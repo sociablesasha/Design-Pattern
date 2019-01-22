e522a1e21f0e9287858e512d83d2e561aa0166f8
Yonghyun Kim <yonghyunkim@yonghyunui-macbookpro.local>
2019-01-22 11:17:39 +0900

    Chain of Responsibility

---
+++ src/Chain_of_Responsibility/OddSupport.java
@@ -0,0 +1,18 @@
+package Chain_of_Responsibility;
+
+public class OddSupport extends Support {
+
+    public OddSupport(String name) {
+        super(name);
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        if (trouble.getNumber() % 2 == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
